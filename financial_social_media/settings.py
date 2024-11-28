2351897dc442ce4657740d26fd35dececdbe9d31
evodata123 <evodata.info@gmail.com>
2024-11-28 20:22:24 +0530

    csrf

--- financial_social_media/settings.py
+++ financial_social_media/settings.py
@@ -70,9 +70,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
